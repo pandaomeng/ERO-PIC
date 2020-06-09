b9be9de6a5bb52d40a0ca1497fd6e1a27599434d
shionjiyusa <876203144@qq.com>
2020-06-09 22:13:25 +0800

    修改 picture 页面

--- src/pages/Picture/service.ts
+++ src/pages/Picture/service.ts
@@ -1,5 +1,27 @@
 import axios from '../../utils/axios';
 
-const getPicture = (pid: number): any => axios(`api/picture?pid=${pid}`);
+export interface PictureInfo {
+  picture: {
+    picture_dir: string;
+    total_score: number;
+    create_by: number;
+    create_time: number;
+    r18: boolean;
+  };
+  [index: number]: Score;
+  [index: number]: Tag;
+}
 
-export default getPicture;
+interface Score {
+  score_id: number;
+  score: number;
+  create_by: number;
+}
+
+interface Tag {
+  tag_id: number;
+  tag: string;
+  create_by: number;
+}
+
+export const getPicture = (pid: number): any => axios(`api/picture?pid=${pid}`);
